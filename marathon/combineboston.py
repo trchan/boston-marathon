3ccec9e220dbea9fc60ea870c3fbdebcf0e5fbc7
Timothy Chan <trchan@gmail.com>
2016-04-19 21:59:55 -0700

    Bug fix for fill_in_missing_splits()

--- marathon/combineboston.py
+++ marathon/combineboston.py
@@ -281,7 +281,8 @@ def fill_in_missing_splits(df):
                   'time30k': 30, 'time35k': 35., 'time40k': 40.,
                   'offltime': 42.195}
 
-    def get_interpolated_time(runner, last_known_split, next_known_split):
+    def get_interpolated_time(runner, split, last_known_split,
+                              next_known_split):
         '''Returns interpolated time
         '''
         distance = split_dist[next_known_split] - split_dist[last_known_split]
@@ -308,7 +309,8 @@ def fill_in_missing_splits(df):
                         next_known_split = next_split
                         break
                 # Interpolate missing value difference
-                df.loc[ix, split] = get_interpolated_time(runner,  last_known_split, next_known_split)
+                df.loc[ix, split] = get_interpolated_time(runner, split,
+                last_known_split, next_known_split)
                 # Change dummy to record missing split
                 df.loc[ix, missed_splits_cols[split_ix]] = True
     return df
